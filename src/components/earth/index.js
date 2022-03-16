c2491173798e0ac4f8dfd1074eab4654082f8a36
Jeremy <than2314@gmail.com>
2022-03-17 03:48:05 +0700

    style: 💄 add cloud

--- src/components/earth/index.js
+++ src/components/earth/index.js
@@ -1,28 +1,32 @@
 import React from "react";
+import { useLoader } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import * as THREE from "three"
+
+import EarthDayMap from "../../assets/textures/8k_earth_daymap.jpg";
+import EarthNomalMap from "../../assets/textures/8k_earth_normal_map.jpg";
+import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg";
+import EarthCloudsMap from "../../assets/textures/8k_earth_clouds.jpg";
+import { TextureLoader } from "three";
 
 export function Earth(props) {
+
+	const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(TextureLoader, [EarthDayMap, EarthNomalMap, EarthSpecularMap, EarthCloudsMap]);
+
 	return (
 		<>
+			<ambientLight intensity={1} />
+			<mesh>
+				<sphereGeometry args={[1.005, 32, 32]} />
+				<meshPhongMaterial map={cloudsMap} opacity={0.4}  depthWrite={true} transparent={true} side={THREE.DoubleSide} />
+			</mesh>
 			<mesh>
 				<sphereGeometry args={[1, 32, 32]} />
-				<meshPhongMaterial color="red" />
+				<meshStandardMaterial specularMap={specularMap} />
+				{/* <meshPhongMaterial color="red" /> */}
+				<meshStandardMaterial map={colorMap} normalMap={normalMap} />
+				<OrbitControls enableZoom={true} enablePan={true} enableRotate={true} zoomSpeed={0.6} panSpeed={0.5} rotateSpeed={0.4} />
 			</mesh>
 		</>
 	)
-	// return (
-	// 	<mesh
-	// 		position={props.position}
-	// 		rotation={props.rotation}
-	// 		scale={props.scale}
-	// 		onClick={props.onClick}
-	// 	>
-	// 		<sphereBufferGeometry attach="geometry" args={[1, 32, 32]} />
-	// 		<meshStandardMaterial
-	// 			attach="material"
-	// 			color={0xffffff}
-	// 			roughness={0.5}
-	// 			metalness={0.5}
-	// 		/>
-	// 	</mesh>
-	// );
 }
