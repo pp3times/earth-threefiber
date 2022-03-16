61380458a90ebcd056d8f97d6da0dab7de9ad353
Jeremy <than2314@gmail.com>
2022-03-17 03:51:18 +0700

    style: 💄 add some star

--- src/components/earth/index.js
+++ src/components/earth/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLoader } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Stars } from "@react-three/drei";
 import * as THREE from "three"
 
 import EarthDayMap from "../../assets/textures/8k_earth_daymap.jpg";
@@ -16,14 +16,14 @@ export function Earth(props) {
 	return (
 		<>
 			<ambientLight intensity={1} />
+			<Stars radius={300} depth={60} count={20000} factor={7} saturation={0} fade={true} />
 			<mesh>
 				<sphereGeometry args={[1.005, 32, 32]} />
-				<meshPhongMaterial map={cloudsMap} opacity={0.4}  depthWrite={true} transparent={true} side={THREE.DoubleSide} />
+				<meshPhongMaterial map={cloudsMap} opacity={0.4} depthWrite={true} transparent={true} side={THREE.DoubleSide} />
 			</mesh>
 			<mesh>
 				<sphereGeometry args={[1, 32, 32]} />
 				<meshStandardMaterial specularMap={specularMap} />
-				{/* <meshPhongMaterial color="red" /> */}
 				<meshStandardMaterial map={colorMap} normalMap={normalMap} />
 				<OrbitControls enableZoom={true} enablePan={true} enableRotate={true} zoomSpeed={0.6} panSpeed={0.5} rotateSpeed={0.4} />
 			</mesh>
